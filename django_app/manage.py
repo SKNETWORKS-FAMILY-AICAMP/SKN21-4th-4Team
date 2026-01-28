053eed34720c629c2ee542dc6d95e80e0cad9c3a
reasonableplan <juwon123465@gmail.com>
2026-01-28 17:28:19 +0900

    경로 수정

--- django_app/manage.py
+++ django_app/manage.py
@@ -7,6 +7,12 @@ import sys
 
 def main():
     """Run administrative tasks."""
+    # 현재 파일(manage.py)의 상위 상위 디렉토리를 sys.path에 추가
+    # 이렇게 하면 'django_app'을 패키지로 인식할 수 있습니다.
+    from pathlib import Path
+    current_path = Path(__file__).resolve().parent
+    sys.path.append(str(current_path.parent))
+
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_app.config.settings")
     try:
         from django.core.management import execute_from_command_line
